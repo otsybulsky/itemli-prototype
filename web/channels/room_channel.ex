57895fd2596fe65b87553440a7fc78a54ad5bda8
otsybulsky <helg37@gmail.com>
2018-04-04 21:19:17 +0300

    test receive data from channel

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,12 +1,12 @@
 defmodule Itemli.RoomChannel do
   use Itemli.Web, :channel
 
-  def join(_name, _params, socket) do
+  def join("room:" <> room_id, _params, socket) do
     {:ok, socket}
   end
 
-  def handle_in(_name, _params, socket) do
-    {:ok, socket}
+  def handle_in("tabs:" <> mode, %{"content" => content}, socket) do
+    {:reply, {:ok, %{content: content}}, socket}
   end
   
 end
