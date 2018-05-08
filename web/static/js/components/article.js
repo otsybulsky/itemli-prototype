c4ad5d648d1cab717f16eabf69f1e853430d9265
otsybulsky <helg37@gmail.com>
2018-05-08 18:29:09 +0300

    start edit article in click to more action

--- web/static/js/components/article.js
+++ web/static/js/components/article.js
@@ -73,36 +73,40 @@ class Article extends Component {
     this.setState({ onHover: false })
   }
   renderEditInterface() {
-    const { onHover } = this.state
-    if (!onHover) return null
-
-    const editInterface = (
-      <div className="tag-body-interface" onClick={ev => this.onEditClick(ev)}>
-        <i className="small material-icons">edit</i>
-      </div>
-    )
-
-    const deleteInterface = (
-      <div
-        className="tag-body-interface"
-        onClick={ev => this.onDeleteClick(ev)}
-      >
-        <i className="small material-icons">delete</i>
-      </div>
-    )
+    // const { onHover } = this.state
+    // if (!onHover) return null
 
     return (
-      <div>
-        {editInterface}
-        {deleteInterface}
-      </div>
+      <i
+        className="material-icons small waves-effect waves-light article-item-more"
+        onClick={ev => this.onEditClick(ev)}
+      >
+        content_paste
+      </i>
     )
+
+    // const deleteInterface = (
+    //   <div
+    //     className="tag-body-interface"
+    //     onClick={ev => this.onDeleteClick(ev)}
+    //   >
+    //     <i className="small material-icons">delete</i>
+    //   </div>
+    // )
+
+    // return (
+    //   <div>
+    //     {editInterface}
+    //     {deleteInterface}
+    //   </div>
+    // )
   }
 
   onEditClick(event) {
     const { article, editArticle } = this.props
     editArticle(article)
     event.stopPropagation()
+    event.preventDefault()
   }
   onDeleteClick(event) {
     const { article, deleteArticle } = this.props
@@ -128,16 +132,16 @@ class Article extends Component {
           onMouseLeave={ev => this.onMouseLeave(ev)}
           style={{ opacity }}
         >
-          <Row>
-            <Col s={1}>
-              <i className="material-icons tiny waves-effect waves-light">
-                content_paste
-              </i>
-            </Col>
-            <Col s={11}>
-              <img className="favicon" src={favicon} />
-              <a href={url} target="_blank">
-                {title}
+          <Row className="article-item">
+            <Col s={12}>
+              <a href={url} className="article-item-title" target="_blank">
+                <div className="article-item-body">
+                  <div className="article-item-title">
+                    {this.renderEditInterface()}
+                    <img className="favicon" src={favicon} />
+                    {title}
+                  </div>
+                </div>
               </a>
             </Col>
           </Row>
