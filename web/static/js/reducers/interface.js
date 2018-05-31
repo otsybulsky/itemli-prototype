6bbd2211f10bf83df1c40cac8500ca1bb9cdf572
otsybulsky <helg37@gmail.com>
2018-05-31 19:57:28 +0300

    get tag_ids for article

--- web/static/js/reducers/interface.js
+++ web/static/js/reducers/interface.js
@@ -1,11 +1,25 @@
-import { DRAG_ELEMENT_START, DRAG_ELEMENT_END } from '../constants'
+import {
+  DRAG_ELEMENT_START,
+  DRAG_ELEMENT_END,
+  ARTICLE_EDIT,
+  ARTICLE_EDIT_ADD_TAG,
+  ARTICLE_EDIT_REMOVE_TAG
+} from '../constants'
 
 const INIT_STATE = {
-  renderDropInterface: null
+  renderDropInterface: null,
+  article_edit_tag_ids: []
 }
 
 export default function(store = INIT_STATE, { type, payload }) {
   switch (type) {
+    case ARTICLE_EDIT:
+      const { tags } = payload
+      return {
+        ...store,
+        article_edit_tag_ids: tags ? tags.map(item => item.id) : []
+      }
+
     case DRAG_ELEMENT_START:
       return { ...store, renderDropInterface: true }
     case DRAG_ELEMENT_END:
