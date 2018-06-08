a545da70f5c92ffb797b1e480ef80d967df9afd7
otsybulsky <helg37@gmail.com>
2018-06-08 10:48:19 +0300

    template form import manual

--- web/static/js/components/import_data.js
+++ web/static/js/components/import_data.js
@@ -1,5 +1,16 @@
-import React from 'react'
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
-const ImportData = () => <p>import data</p>
+class ImportData extends Component {
+  render() {
+    return (
+      <div>
+        <h5>Import data manual</h5>
+        <h6>Tag title</h6>
+        <h6>URL's list</h6>
+      </div>
+    )
+  }
+}
 
 export default ImportData
