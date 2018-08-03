c3f43d3aef8ec4189468c02d81642d695cadd4af
otsybulsky <helg37@gmail.com>
2018-08-03 20:09:37 +0300

    test deploy to gigalixir

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :itemli, Itemli.Endpoint,
   load_from_system_env: true,
-  url: [host: "localhost", port: 4000],
+  url: [host: "example.com", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
