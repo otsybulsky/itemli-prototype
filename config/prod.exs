33b783f5854774d7a0c61764b2a251c854ce51b8
otsybulsky <helg37@gmail.com>
2018-08-04 10:06:23 +0300

    config  buildpack

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,7 @@ use Mix.Config
 
 config :itemli, Itemli.Endpoint,
   load_from_system_env: true,
-  url: [host: "example.com", port: 80],
+  url: [host: "localhost", port: 4000],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
